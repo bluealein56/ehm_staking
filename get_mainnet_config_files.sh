1c586e49a3d2639845a9fec02f58931b0d736121
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-05 10:23:50 -0500

    updating file paths for config files

--- get_mainnet_config_files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Downloading mainnet files..."
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json -O ./config/config.json
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json -O ./config/byron-genesis.json
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json -O ./config/shelley-genesis.json
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json -O ./config/topology.json
-
-
