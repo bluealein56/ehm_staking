dbe47e6f961f7a27f631c75e2c3f90fdfdc9a3e9
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-05 13:07:17 -0500

    update dockerfile to pull from private image repo

--- containers/cardano-node/build.sh
+++ containers/cardano-node/build.sh
@@ -1,8 +1,20 @@
-if [ -z "$1" ]; then
-    TAG="latest"
-else
-    TAG=$1
-fi
-#docker login registry.gitlab.com
-docker build -t bluealein56/ehm:$TAG .
-docker push bluealein56/ehm:$TAG
+#if [ -z "$1" ]; then
+#    TAG="latest"
+#else
+#    TAG=$1
+#fi
+##docker login registry.gitlab.com
+#docker build -t bluealein56/ehm:$TAG .
+#docker push bluealein56/ehm:$TAG
+#
+docker login
+docker build -t ehm-node:latest .
+docker tag ehm-node:latest bluealein56/ehm:ehm-node
+#docker push bluealein56/ehm:$TAG
+docker push bluealein56/ehm:ehm-node
+
+https://github.com/bluealein56/ehm_staking/blob/main/containers/debian-base/Dockerfile
+
+docker login registry.gitlab.com
+docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG .
+docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG
