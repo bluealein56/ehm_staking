b84e747a18b3e83252dfe2c2dee8f58678a13aea
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-05 12:14:06 -0500

    update Dockerfile and build file with correct build, tag and push params

--- containers/debian-base/build.sh
+++ containers/debian-base/build.sh
@@ -1,5 +1,5 @@
 docker login
-docker build -t ehm1:latest .
-docker tag ehm1:latest bluealein56/ehm:ehm1
+docker build -t ehm-base:latest .
+docker tag ehm-base:latest bluealein56/ehm:ehm-base
 #docker push bluealein56/ehm:$TAG
-docker push bluealein56/ehm:ehm1
+docker push bluealein56/ehm:ehm-base
