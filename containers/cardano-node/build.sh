3c7e78d2f3a43734626feb4b386ccef9f7e7a102
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-05 22:53:24 -0500

    updating docker repo names and structure

--- containers/cardano-node/build.sh
+++ containers/cardano-node/build.sh
@@ -7,14 +7,11 @@
 #docker build -t bluealein56/ehm:$TAG .
 #docker push bluealein56/ehm:$TAG
 #
-docker login
+#docker login
 docker build -t ehm-node:latest .
 docker tag ehm-node:latest bluealein56/ehm:ehm-node
 #docker push bluealein56/ehm:$TAG
 docker push bluealein56/ehm:ehm-node
 
-https://github.com/bluealein56/ehm_staking/blob/main/containers/debian-base/Dockerfile
 
-docker login registry.gitlab.com
-docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG .
-docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG
+
--- containers/debian-base/build.sh
+++ containers/debian-base/build.sh
@@ -1,5 +1,5 @@
 docker login
-docker build -t ehm-base:latest .
-docker tag ehm-base:latest bluealein56/ehm:ehm-base
+docker build -t debian-base:latest .
+#docker tag debian-base:latest bluealein56/ehm:debian-base
 #docker push bluealein56/ehm:$TAG
-docker push bluealein56/ehm:ehm-base
+docker push bluealein56/debian-base:latest
